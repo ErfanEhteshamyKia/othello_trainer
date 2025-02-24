eb74cecdac5abfa14e044391c078d7211bf7e128
houdini <erfanehteshamykia@gmail.com>
2025-02-24 20:24:01 +0330

    inital commit

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const cacheName = 'othello-puzzle-trainer-v1';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/game.png',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then((cache) => {
+                return cache.addAll(assetsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                return cachedResponse || fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [cacheName];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
